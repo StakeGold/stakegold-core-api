7fe5769656f7bdb81c9fed47690b4612c32d53a8
Marius Stoica <stoica.nmarius@yahoo.ro>
2022-09-23 16:06:17 +0300

    Removed forwarder

--- lib/endpoints/elrond-communication/elrond-proxy.service.d.ts
+++ lib/endpoints/elrond-communication/elrond-proxy.service.d.ts
@@ -1,7 +1,6 @@
 import { ProxyNetworkProvider } from '@elrondnetwork/erdjs-network-providers/out';
 import { SmartContractProfiler } from '../utils/smartcontract.profiler';
 export interface StakeGoldElrondProxyService {
-    getForwarderSmartContract(shard: number): Promise<SmartContractProfiler>;
     getService(): ProxyNetworkProvider;
     getFarmSmartContract(farmAddress: string): Promise<SmartContractProfiler>;
     getRouterSmartContract(): Promise<SmartContractProfiler>;
