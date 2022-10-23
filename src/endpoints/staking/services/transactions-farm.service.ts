05ab2581f000a1595daefb99ce4095c3901d9f72
Marius Stoica <stoica.nmarius@yahoo.ro>
2022-10-23 14:48:45 +0300

    Return multiple transactions when there are more tokens

--- src/endpoints/staking/services/transactions-farm.service.ts
+++ src/endpoints/staking/services/transactions-farm.service.ts
@@ -211,6 +211,18 @@ export class TransactionsFarmService {
       throw new BadRequestException('No tokens sent');
     }
 
+    if (stakingArgs.tokens.length > 1) {
+      return this.contextTransactions.multiESDTNFTTransfer(
+        new Address(sender),
+        contract,
+        stakingArgs.tokens,
+        method,
+        [],
+        gasLimit,
+        this.apiConfigService.getChainId(),
+      );
+    }
+
     return this.contextTransactions.nftTransfer(
       new Address(sender),
       contract,
