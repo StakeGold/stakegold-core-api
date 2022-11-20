6f0f64f9653c8ef44654e8d48344ba4b510cbfdd
Marius Stoica <stoica.nmarius@yahoo.ro>
2022-11-20 18:30:54 +0200

    Version bump

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -2,7 +2,6 @@ export * from './account';
 export * from './api-config';
 export * from './context';
 export * from './elrond-communication';
-export * from './generics';
 export * from './meta-esdt';
 export * from './proxy';
 export * from './staking';
